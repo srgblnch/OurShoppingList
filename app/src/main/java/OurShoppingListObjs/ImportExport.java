c65aab173abf40fff86d6f34144179c0a41134fb
Sergi Blanch-Torné <sergi@calcurco.cat>
2017-05-10 19:20:17 +0200

    working to have the import/export task in background. Specially to show a progress bar to have the user updated about the process.

--- app/src/main/java/OurShoppingListObjs/ImportExport.java
+++ app/src/main/java/OurShoppingListObjs/ImportExport.java
@@ -1,6 +1,10 @@
 package OurShoppingListObjs;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
 import java.io.File;
+
 
 import OurShoppingListDataBase.OurData;
 
@@ -8,21 +12,62 @@ import OurShoppingListDataBase.OurData;
  * Created by serguei on 24/03/17.
  */
 
+//public class ImportExport<Boolean> extends AsyncTaskLoader<Boolean> {
 public class ImportExport {
-    final static String TAG = "ImportExport";
+    private String TAG = new String("ImportExport");
+    //private enum Action {IMPORT, EXPORT};
+    //Action action;
+    //File directory, file;
+    //String fileName;
 
-    public ImportExport() {
-
+    public ImportExport(Context context) {
+        //super(context);
     }
 
+//    @Override
+//    protected void onStartLoading() {
+//
+//    }
+
+//    @Override
+//    public Boolean loadInBackground() {
+//        Boolean result;
+//        OurData db = OurData.getInstance();
+//        if ( action == Action.IMPORT ) {
+//            //return Boolean.valueOf(db.doImport("csv", file));
+//        } else if ( action == Action.EXPORT ) {
+//            //result = Boolean.valueOf(db.doExport("csv", directory, fileName));
+//        }
+//        return result;
+//    }
+
+//    @Override
+//    public void deliverResult(final Boolean data) {
+//
+//        super.deliverResult(data);
+//    }
+
+//    @Override
+//    protected void onReset() {
+//        super.onReset();
+//
+//    }
+
     public boolean exportDB2CSV(File directory, String fileName) {
+//        action = Action.EXPORT;
+//        this.directory = directory;
+//        this.fileName = fileName;
         OurData db = OurData.getInstance();
         return db.doExport("csv", directory, fileName);
+//        return loadInBackground();
     }
 
     public boolean importDB2CSV(File file) {
+//        action = Action.IMPORT;
+//        this.file = file;
         OurData db = OurData.getInstance();
         return db.doImport("csv", file);
+//        return loadInBackground();
     }
 }
 
